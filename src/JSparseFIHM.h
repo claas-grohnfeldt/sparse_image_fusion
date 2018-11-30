377c72702ecf26d481250e5cdacf190b5a0f10cc
claasg <claas.grohnfeldt@gmail.com>
2018-11-30 10:02:01 +0100

    renamed and updated

---
+++ src/JSparseFIHM.h
@@ -0,0 +1,27 @@
+/*
+ * JSparseFIHM.h
+ *
+ *  Created on: Apr 30, 2013
+ *      Author: Claas Grohnfeldt
+ */
+
+#ifndef JSPARSEFIHM_H_
+#define JSPARSEFIHM_H_
+
+// project headers
+#include "includes.h"
+
+#include "dataIO.h"
+#include "auxFcts.h"
+#include "eval_alg.h"
+#include "userSettings.h"
+#include "JSparseFIHM_alg.h"
+
+#include "JS.h"
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+
+#endif /* JSPARSEFIHM_H_ */
---
+++ src/JSparseFIHM_alg.h
@@ -0,0 +1,42 @@
+/*
+ * JSparseFIHM_alg.h
+ *
+ *  Created on: Feb 20, 2014
+ *      Author: Claas Grohnfeldt
+ */
+
+#ifndef JSPARSEFIHM_ALG_H_
+#define JSPARSEFIHM_ALG_H_
+
+#include "includes.h"
+
+// project headers
+#include "dataIO.h"
+#include "JS.h"
+#include "mpi_counter.h"
+#include "auxFcts.h"
+
+void JSparseFIHM_alg(	                SpEOMatrixD &EndmemberMat, 
+					SpEOMatrixD* &AbundanceMat, 
+                                        int iterMain,
+					int numIterMain,
+					SpEOPaths *paths,
+					SpEODataIOSetting *dSet,
+					SpEOFusionSetting *fSet,
+					SpEOOutputSetting *oSet,
+					SpEOSolverSetting *sSet,
+					SpEOParallelSetting *pSet,
+					SpEOGlobalParams *glPrms,
+					SpEODataset *ImX,
+					SpEODataset *ImX_LR,
+					SpEODataset *ImY,
+					SpEODataset *ImZ,
+					SpEOMatrixD *SRF,
+					MPI_Comm comm_busy,
+					MPI_Group group_busy,
+					SpEODataset *ImZ_init,
+					SpEOReport &report);
+
+void full_im_optimization_LS(SpEOReport &report, SpEOMatrixD &EndmemberMat, SpEOMatrixD* &AbundanceMat, SpEOGlobalParams *glPrms, SpEOFusionSetting *fSetting, SpEOSolverSetting *sSetting, SpEODataIOSetting *dSetting, SpEODataset *ImX, SpEODataset *ImY, SpEODataset *ImZ, SpEOMatrixD *SRF, MPI_Comm comm_busy, MPI_Group group_busy);
+ 
+#endif /* JSPARSEFIHM_ALG_H_ */
